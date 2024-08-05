075007947033c479e6837e460791ce697e217c2d
Utku Acar <utekau@gmail.com>
2024-08-05 21:21:25 +0300

    More data inserted at app startup

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -9,3 +9,7 @@ INSERT INTO stock (name, description, current_price, last_update) VALUES ('Stock
 INSERT INTO stock (name, description, current_price, last_update) VALUES ('Stock C', '', 1000.00, CURRENT_TIMESTAMP());
 INSERT INTO stock (name, description, current_price, last_update) VALUES ('Stock D', '', 1000000.00, CURRENT_TIMESTAMP());
 INSERT INTO stock (name, description, current_price, last_update) VALUES ('Stock E', '', 250.50, CURRENT_TIMESTAMP());
+
+INSERT INTO stock_exchange_stocks (stock_exchange_id, stock_id) VALUES (1, 3);
+INSERT INTO stock_exchange_stocks (stock_exchange_id, stock_id) VALUES (1, 1);
+INSERT INTO stock_exchange_stocks (stock_exchange_id, stock_id) VALUES (2, 1);
